a6a5897ecaf269c01ead1f4f127f0993dd7b924b
bogdan <helpdesk@gerchikco.com>
2019-02-24 18:05:23 +0200

    add planet to component, get it random

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -6,7 +6,17 @@ export default class ItemList extends Component {
 
     render() {
         return (
-            <div>Item List</div>
-        )
+            <ul className="item-list list-group">
+                <li className="list-group-item">
+                    Luke Skywalker
+                </li>
+                <li className="list-group-item">
+                    Darth Vader
+                </li>
+                <li className="list-group-item">
+                    R2-D2
+                </li>
+            </ul>
+        );
     }
 }
