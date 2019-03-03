94f6f58acbd6060d08fe89454a60944efd68f18a
bogdan <helpdesk@gerchikco.com>
2019-03-03 18:13:21 +0200

    fix age, color

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -83,8 +83,8 @@ export default class SwapiService extends Component {
             id: this._extractId(person),
             name: person.name,
             gender: person.gender,
-            birthYear: person.birthYear,
-            eyeColor: person.eyeColor
+            birthYear: person.birth_year,
+            eyeColor: person.eye_color
         };
     };
 }
